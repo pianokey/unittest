f1a6ba464ef884931719068ad91d8ad856e97139
Liang Mingyi <liangmingyi@baidu.com>
2013-09-28 14:17:29 +0800

    Add test case

--- test/index.test.js
+++ test/index.test.js
@@ -11,4 +11,47 @@ describe('module', function() {
 			lib.limit(-1).should.be.equal(0);
 		});
 	});
+
+	describe('async', function() {
+		it('async', function(done) {
+			lib.async(function(result) {
+				done();
+			});
+		});
+	});
+
+	describe('asyncTimeout', function() {
+		it('async should success', function(done) {
+			lib.asyncTimeout(function(result) {
+				done();
+			});
+		});
+	});
+
+	describe('parseAsync', function () {
+		it('parseAsync should ok', function (done) {
+			lib.parseAsync('{"name": "LiangMingyi"}', function (err, data) {
+				should.not.exist(err);
+				data.name.should.be.equal('LiangMingyi');
+				done();
+			});
+		});
+
+		it('parseAsync should throw err', function (done) {
+			lib.parseAsync('{"name": "LiangMingyi"}}', function (err, data) {
+				should.exist(err);
+				done();
+			});
+		});
+	});
+
+	describe('getContent', function () {
+		it('getContent should ok', function (done) {
+			lib.getContent(__filename, function (err, content) {
+				should.not.exist(err);
+				content.length.should.be.above(100);
+				done();
+			});
+		});
+	})
 });
